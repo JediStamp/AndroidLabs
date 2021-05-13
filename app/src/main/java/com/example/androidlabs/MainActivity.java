8ef2ded576c2efd126eca16676bf7c2b2dad3a3a
Jenne <jkmiklos@hotmail.com>
2021-05-13 14:55:30 -0400

    Lab1 - complete

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonName;
+        buttonName = (Button) findViewById(R.id.button);
+        buttonName.setOnClickListener((v) ->  {
+            finish();
+            System.exit(0);
+        });
     }
 }
