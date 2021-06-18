d43c96712c9cb1dac729bad499756392735ddd1b
Jenne <jkmiklos@hotmail.com>
2021-06-17 21:11:44 -0400

    Lab6 - base

--- app/src/main/java/com/example/androidlabs/WeatherForcastActivity.java
+++ app/src/main/java/com/example/androidlabs/WeatherForcastActivity.java
@@ -176,8 +176,6 @@ public class WeatherForcastActivity extends AppCompatActivity {
             catch (Exception e) {
                 Log.e("Error", e.getMessage());
             }
-
-
             return "Done";
         }
 
